c69928dff51664a3c01206c147281bcd82aaa66f
Ed Nutting <ed@beyondrisc.com>
2020-08-08 18:49:35 +0100

    GStreamer demo working

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
-use std::path::Path;
+extern crate gstreamer as gst;
+use gst::prelude::*;
 
-extern crate camera_capture;
+fn tutorial_main() {
+    // Initialize GStreamer
+    gst::init().unwrap();
+
+    // Build the pipeline
+    let uri =
+        "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm";
+    let pipeline = gst::parse_launch(&format!("playbin uri={}", uri)).unwrap();
+
+    // Start playing
+    pipeline
+        .set_state(gst::State::Playing)
+        .expect("Unable to set the pipeline to the `Playing` state");
+
+    // Wait until error or EOS
+    let bus = pipeline.get_bus().unwrap();
+    for msg in bus.iter_timed(gst::CLOCK_TIME_NONE) {
+        use gst::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.get_src().map(|s| s.get_path_string()),
+                    err.get_error(),
+                    err.get_debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    // Shutdown pipeline
+    pipeline
+        .set_state(gst::State::Null)
+        .expect("Unable to set the pipeline to the `Null` state");
+}
 
 fn main() {
-  let cam = camera_capture::create(0).unwrap();
-  let cam = cam.fps(5.0).unwrap().start().unwrap();
-  for image in cam {
-    let path = Path::new("./image.jpg");
-    let _ = image.save(path);
-    println!("frame");
-    break;
-  }
-  println!("done");
+    tutorial_main();
 }
