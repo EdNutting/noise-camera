0d8e2a9cbcc0dbc41e0c52920192a40de1392413
Ed Nutting <ed@beyondrisc.com>
2020-08-08 16:54:27 +0100

    Proof on concept: Save an image from the webcam

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::Path;
+
+extern crate camera_capture;
+
 fn main() {
-    println!("Hello, world!");
+  let cam = camera_capture::create(0).unwrap();
+  let cam = cam.fps(5.0).unwrap().start().unwrap();
+  for image in cam {
+    let path = Path::new("./image.jpg");
+    let _ = image.save(path);
+    println!("frame");
+    break;
+  }
+  println!("done");
 }
